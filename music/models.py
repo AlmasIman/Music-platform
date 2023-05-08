a5bf901ee7baf5145840a0c555c32557d8a726eb
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-05-08 18:13:34 +0600

    Update models.py

--- music/models.py
+++ music/models.py
@@ -11,24 +11,25 @@ class Singer(models.Model):
     # image = models.ImageField(upload_to="images",\
     #                           help_text="The image of the singer")
 
+class Genre(models.Model):
+    name = models.CharField \
+        (max_length= 50,\
+         help_text= "The genre")
+    
 class Song(models.Model):
     name = models.CharField \
         (max_length= 50,\
          help_text="The name of the song")
     singer = models.ForeignKey \
         (Singer, on_delete=models.CASCADE)
-    genre = models.ForeignKey \
+    genre = models.ForeignKey\
         (Genre, on_delete=models.CASCADE)
-#      song_date = models.DateField \
-#         (verbose_name=\
-#         "Date the song was released")
+    # song_date = models.DateField \
+    #     (verbose_name= \
+    #          "Date the song was released",\
+    #      default=2023)
     # image = models.ImageField(upload_to="images", \
     #                           help_text="The image of the song")
-
-class Genre(models.Model):
-    name = models.CharField \
-        (max_length= 50,\
-         help_text= "The genre")
 
 class User(models.Model):
     nickname = models.CharField \
