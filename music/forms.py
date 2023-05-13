46e0f13ee5ebbdc46611c9cf70d76832b8b2aa43
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-05-14 00:50:11 +0600

    Update forms.py
    +signupform

--- music/forms.py
+++ music/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from .models import Song
 from django.forms import ModelForm, TextInput
 from .models import Song, Singer
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class SongForm(forms.ModelForm):
     class Meta:
         model = Song
         fields = ['name', 'singer', 'genre', 'year', 'image', 'song']
+        
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
