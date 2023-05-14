77eb35c5da8747432e403480376137a50adda489
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-05-14 22:56:09 +0600

    Update settings.py
    lines 140-146 about password-reset

--- music_platform/settings.py
+++ music_platform/settings.py
@@ -136,3 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'main'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'useremail'
+EMAIL_HOST_PASSWORD = 'password'
