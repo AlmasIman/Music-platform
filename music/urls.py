1798ef0d5a78f7964ceab15d76d1dc6c85683923
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2023-05-14 22:49:45 +0600

    Update urls.py
    added reset-password

--- music/urls.py
+++ music/urls.py
@@ -22,6 +22,18 @@ urlpatterns = [
     path('favourites/', views.favourites, name='favorites'),
     path('like_song/<int:song_id>/', views.like_song, name='like_song'),
     path('unlike_song/<int:song_id>/', views.unlike_song, name='unlike_song'),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"),
+         name="reset_password"),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"),
+         name="password_reset_complete"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
